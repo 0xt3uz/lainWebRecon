0a7e1ad4872fe85b7fa2678b3d43b5be56d18ec3
0xt3uz <ma7hcavl@gmail.com>
2023-06-06 09:52:39 -0300

    Added function to filter addresses in HTML

--- lainWebRecon.sh
+++ lainWebRecon.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-arquivo=$2
 op=$1
-url=$3
+arquivo=$3
+url=$2
 sc=$4
+
+if [ $3 ]
+then
+	arquivo=$3
+else
+	arquivo=0
+fi
+
+if [ $4 ]
+then
+	sc=$4
+else
+	sc=0
+fi
 
 if [ $5 ]
 then
@@ -15,10 +29,17 @@ fi
 usage_func(){
 	echo -e "Usage: ./lainWebRecon <op> <url> <wordlist> <http_status_code> <ext>\n"
 
-	echo "<op>: dir ou files"
-	echo "<url>: host url"
+	echo "<op>: dir, files, addressearch"
+	echo "<url>: url"
 	echo "<wordlist>: lista com nomes"
+	echo "<http_status_code>: código HTTP de retorno"
 	echo "<ext>: extensão para busca (útil apenas em casos de busca por arquivos)"
+
+	echo -e "\n"
+
+	echo "Para realizar brute force de diretórios: ./lainWebRecon dir google.com wordlist 200"
+	echo "Para realizar brute force de arquivos: ./lainWebRecon file google.com wordlist 200 txt"
+	echo "Para realizar busca de emails ./lainWebRecon addressearch google.com"
 }
 
 dir_func(){
@@ -41,13 +62,27 @@ files_func(){
 	done < $arquivo
 }
 
+addressearch_func(){
+	wget -q $url
+	grep href index.html | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<l" > hosts.txt
+
+	for host in $(cat hosts.txt); do
+		echo "Endereço Encontrado => $host"
+	done
+
+	rm hosts.txt
+	rm index.html
+}
 
 if [[ $op == "dir" ]]
 then
 	dir_func
-elif [[ $op == "file" ]];
+elif [[ $op == "file" ]]
 then
 	files_func
+elif [[ $op == "addressearch" ]]
+then
+	addressearch_func
 else
 	usage_func
 fi
